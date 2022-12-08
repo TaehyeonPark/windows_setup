5771869d11dbbe7e948b072f94b382e0f7dccebb
TaehyeonPark <84885011+TaehyeonPark@users.noreply.github.com>
2022-12-08 20:10:16 +0900

    Update mongoose_WebServerLib_in_C.ps1

--- mongoose_WebServerLib_in_C.ps1
+++ mongoose_WebServerLib_in_C.ps1
@@ -1,4 +1,27 @@
-mkdir mongoose
-cd mongoose
-wget https://raw.githubusercontent.com/cesanta/mongoose/master/mongoose.c
-wget https://raw.githubusercontent.com/cesanta/mongoose/master/mongoose.h
+$path = "mongoose"
+
+Function Check-Role
+{
+  Param( [Security.Principal.WindowsBuiltinRole]$Role )
+
+  $CurrentUser = [Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())
+
+  $CurrentUser.IsInRole($Role)
+}
+
+if ( Check-Role "Administrator" )
+{
+	echo "Setup start..."
+	echo "Files will be installed under './$path/'"
+	mkdir mgs
+	wget -Uri https://raw.githubusercontent.com/cesanta/mongoose/master/mongoose.c -OutFile ./$path/mongoose.c
+	wget -Uri https://raw.githubusercontent.com/cesanta/mongoose/master/mongoose.h -OutFile ./$path/mongoose.h
+	ls $path
+}
+else
+{
+	echo "Try again with Administrator Auth..."
+}
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
